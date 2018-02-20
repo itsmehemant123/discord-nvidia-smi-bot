34dde35a9c3cf82685cbadec20c6a267467dd5c4
Hemanth Devarapalli <itsmehemant123@gmail.com>
2018-02-19 20:53:25 -0500

    Stupid types;S

--- nvidia.py
+++ nvidia.py
@@ -29,16 +29,16 @@ class NvidiaGPUInfoBot:
             dName = nvmlDeviceGetName(dHandle)
             # dMemInfo = nvmlDeviceGetMemoryInfo(dHandle) #total, free, used
             dUtil = nvmlDeviceGetUtilizationRates(dHandle) # gpu, memory
-            gpuInfo += "*GPU Usage*: " + dUtil.gpu + "%\n*Memory Usage*: " + dUtil.memory + "%\n"
+            gpuInfo += "*GPU Usage*: " + str(dUtil.gpu) + "%\n*Memory Usage*: " + str(dUtil.memory) + "%\n"
             dFanSpeed = nvmlDeviceGetFanSpeed(dHandle) # print with %
-            gpuInfo += "*Fan Speed*: " + dFanSpeed + "%\n"
+            gpuInfo += "*Fan Speed*: " + str(dFanSpeed) + "%\n"
             dPerfState = nvmlDeviceGetPerformanceState(dHandle)
-            gpuInfo += "*Perf. State*: " + dPerfState + "\n"
+            gpuInfo += "*Perf. State*: " + str(dPerfState) + "\n"
             dGPUTemps = nvmlDeviceGetTemperature(dHandle, NVML_TEMPERATURE_GPU) # prints in Celcius
-            gpuInfo += "*Temperature*: " + dGPUTemps + "C\n"
+            gpuInfo += "*Temperature*: " + str(dGPUTemps) + "C\n"
             dCurrentWattage = nvmlDeviceGetPowerUsage(dHandle) # divide by 1000 to get wattage
             dMaxWattage = nvmlDeviceGetEnforcedPowerLimit(dHandle) # same as above
-            gpuInfo += "*Wattage*: " + (dCurrentWattage/1000) + "W / " + (dMaxWattage/1000) + "W\n"
+            gpuInfo += "*Wattage*: " + str(dCurrentWattage/1000) + "W / " + str(dMaxWattage/1000) + "W\n"
             # dProcs = nvmlDeviceGetComputeRunningProcesses(dHandle)
 
             usage.add_field(name=dName, value=gpuInfo, inline=True)
