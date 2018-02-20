25c815cae26f7da288175f414ea69b72f5926bd5
Hemanth Devarapalli <itsmehemant123@gmail.com>
2018-02-19 20:51:20 -0500

    Added nvml parsing;

--- nvidia.py
+++ nvidia.py
@@ -1,7 +1,10 @@
 import discord
+import datetime
 from discord.ext import commands
 import logging
 import subprocess
+from pynvml import *
+
 
 class NvidiaGPUInfoBot:
 
@@ -11,6 +14,41 @@ class NvidiaGPUInfoBot:
 
     @commands.command(pass_context=True, no_pm=True)
     async def info(self, ctx):
-        result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE)
-        await self.bot.send_message(ctx.message.channel, result.stdout)
+        nvmlInit()
+        
+        deviceCount = nvmlDeviceGetCount()
+        usage = discord.Embed(title='GPU Usage',
+                           description='My Embed Content.', colour=0x00CC33, timestamp=datetime.datetime.now())
+
+
+        for i in range(deviceCount):
+            dHandle = nvmlDeviceGetHandleByIndex(i)
+
+            gpuInfo = ""
+
+            dName = nvmlDeviceGetName(dHandle)
+            # dMemInfo = nvmlDeviceGetMemoryInfo(dHandle) #total, free, used
+            dUtil = nvmlDeviceGetUtilizationRates(dHandle) # gpu, memory
+            gpuInfo += "*GPU Usage*: " + dUtil.gpu + "%\n*Memory Usage*: " + dUtil.memory + "%\n"
+            dFanSpeed = nvmlDeviceGetFanSpeed(dHandle) # print with %
+            gpuInfo += "*Fan Speed*: " + dFanSpeed + "%\n"
+            dPerfState = nvmlDeviceGetPerformanceState(dHandle)
+            gpuInfo += "*Perf. State*: " + dPerfState + "\n"
+            dGPUTemps = nvmlDeviceGetTemperature(dHandle, NVML_TEMPERATURE_GPU) # prints in Celcius
+            gpuInfo += "*Temperature*: " + dGPUTemps + "C\n"
+            dCurrentWattage = nvmlDeviceGetPowerUsage(dHandle) # divide by 1000 to get wattage
+            dMaxWattage = nvmlDeviceGetEnforcedPowerLimit(dHandle) # same as above
+            gpuInfo += "*Wattage*: " + (dCurrentWattage/1000) + "W / " + (dMaxWattage/1000) + "W\n"
+            # dProcs = nvmlDeviceGetComputeRunningProcesses(dHandle)
+
+            usage.add_field(name=dName, value=gpuInfo, inline=True)
+        
+        nvmlShutdown()
+
+        usage.description = "Make sure to check that both GPU and Memory Utilization are 0 before running a task."
+        
+        await self.bot.send_message(ctx.message.channel, embed=usage)
+
+    def shutdown(self):
+        nvmlShutdown()
         
